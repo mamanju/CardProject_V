263fbfa09185b0182e73f8bbdd2e4a2bbc8bf965
mamanju <himazin19970630@gmail.com>
2019-02-28 03:07:43 +0900

    タスク管理書、カード情報の構造体を作成

---
+++ CardProject/Assets/Script/CardInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// カード情報
+/// </summary>
+public struct cardInfo {
+    public string name;         // ユニット名
+    public string cran;         // クラン
+    public string race;         // 種族
+    public enum unitType        // ユニット種類
+    {
+        trigger,
+        normal,
+    }
+    public enum skillIcon       // スキルアイコン
+    {
+        boost,
+        intercept,
+        twinDrive,
+    }
+    public int grade;           // グレード
+    public int power;           // パワー
+    public int shield;          // シールド
+    public int critical;        // クリティカル
+    
+}
+
+/// <summary>
+/// カード情報クラス
+/// </summary>
+public class CardInfo :MonoBehaviour{
+    private cardInfo info;
+    public cardInfo getCardInfo {
+        get { return info; }
+    }
+}
+
---
+++ CardProject/Assets/Script/DeckManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// デッキ統括スクリプト
+/// </summary>
+public class DeckManager : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
